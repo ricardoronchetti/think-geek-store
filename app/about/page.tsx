cf7cbe7fcbf113171d0a20fd8d1a63535fbd4efb
Ricardo Ronchetti <ricardoronchetti@gmail.com>
2025-02-26 13:20:10 +0100

    updated(about): added new text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,16 +9,16 @@ function AboutPage() {
       </h1>
       <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
         Three out of the four founding ThinkGeek members started an ISP in the
-        Northern Virginia area way way back in 1995. We couldn't afford Solaris,
-        learned about a free UNIX-like OS, and spent almost an entire day
-        downloading it onto over 50 floppies for installation on an old 486
+        Northern Virginia area way way back in 1995. We couldn&apos;t afford
+        Solaris, learned about a free UNIX-like OS, and spent almost an entire
+        day downloading it onto over 50 floppies for installation on an old 486
         laptop with no cd-rom (thanks Slackware!). After a few years with the
         ISP gig, the ThinkGeek idea popped into our heads, and, operating out of
         a spare room at the ISP office we setup shop and launched the site on
         Friday the 13th, 1999... All the founding members are still in town, and
-        we've continued to grow our product line, expand our staff (including
-        several canines) and enhance the way we interface with customers. Who
-        would have thought...
+        we&apos;ve continued to grow our product line, expand our staff
+        (including several canines) and enhance the way we interface with
+        customers. Who would have thought...
       </p>
     </section>
   );
